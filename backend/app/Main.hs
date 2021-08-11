0c952d43d6a3664f18a4b5f48a72e200b303262b
Vladimir <vv90@mechmind.net>
2021-08-11 21:06:35 +0700

    added post notes endpoint
    Co-authored-by: VladimirLogachev <logachev.dev@ya.ru>

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -25,6 +25,7 @@ import Database.PostgreSQL.Typed.Protocol (PGConnection)
 import Database.PostgreSQL.Typed.Query (PGSimpleQuery, pgQuery, pgSQL)
 import Database.PostgreSQL.Typed.Types (PGType)
 import GHC.Generics (Generic)
+import GHC.Records
 import qualified Network.HTTP.Types as HttpTypes
 import Web.Spock
   ( HasSpock (getState),
@@ -51,11 +52,22 @@ useTPGDatabase db -- compile time connection
 -- | List of all our domain events in a single event log
 data DomainEvent
   = UserRegistered {email :: Text}
-  | TextChanged {updatedText :: Text}
+  | NoteAdded {userId :: Int32, content :: Text}
+  | NoteRemoved {userId :: Int32, noteId :: Int32}
+  | NoteUpdated {userId :: Int32, noteId :: Int32, content :: Text}
   deriving stock (Eq, Show, Read, Generic)
   deriving anyclass (Aeson.ToJSON, Aeson.FromJSON)
 
-data RegisterUserPayload = RegisterUserPayload {email :: Text, uuid :: UUID.UUID}
+newtype RegisterUserPayload = RegisterUserPayload {email :: Text}
+  deriving stock (Eq, Show, Read, Generic)
+  deriving anyclass (Aeson.ToJSON, Aeson.FromJSON)
+
+newtype NoteAddedPayload = NoteAddedPayload {content :: Text}
+  deriving stock (Eq, Show, Read, Generic)
+  deriving anyclass (Aeson.ToJSON, Aeson.FromJSON)
+
+
+data WithUuid a = WithUuid {payload :: a, uuid :: UUID.UUID}
   deriving stock (Eq, Show, Read, Generic)
   deriving anyclass (Aeson.ToJSON, Aeson.FromJSON)
 
@@ -82,9 +94,9 @@ runQuery q = do
 listEvents :: IO (Aeson.Result [DomainEvent])
 listEvents = do
   traverse Aeson.fromJSON <$> (runQuery [pgSQL| SELECT body from events; |] :: IO [Aeson.Value])
-
-insertEvent :: UUID.UUID -> Aeson.Value -> PGSimpleQuery ()
-insertEvent uuid body = [pgSQL| INSERT INTO events (uuid, body) VALUES (${uuid}, ${body}); |]
+  
+insertEvent :: UUID.UUID -> DomainEvent -> PGSimpleQuery ()
+insertEvent uuid body = [pgSQL| INSERT INTO events (uuid, body) VALUES (${uuid}, ${Aeson.toJSON body}); |]
 
 handleRequests :: IO ()
 handleRequests =
@@ -95,21 +107,41 @@ handleRequests =
 
 app :: SpockM () MySession () ()
 app = do
+
   get root $ text "Hello World!"
+
+
   get "events" $ do
     evts <- liftIO listEvents
     case evts of
       Aeson.Error err -> setStatus HttpTypes.status500
       Aeson.Success xs -> text $ T.pack $ unwords $ map show xs
-  post "events" $ do
+
+
+  post "users" $ do
     body <- jsonBody
     let resultOfPayload = case body of
-          Just validJson -> Aeson.fromJSON @RegisterUserPayload validJson
+          Just validJson -> Aeson.fromJSON @(WithUuid RegisterUserPayload) validJson
           _ -> fail "Json body expected. Status 400" -- TODO: fail with 400
-    let resultOfValue = fmap (Aeson.toJSON . (email :: RegisterUserPayload -> Text)) resultOfPayload
-    let resultOfUuid = fmap (uuid :: RegisterUserPayload -> UUID.UUID) resultOfPayload
-    case (resultOfValue, resultOfUuid) of
-      (Aeson.Success value, Aeson.Success uuid) -> do
-        liftIO $ runQuery $ insertEvent uuid value
+    let resultOfEmail = (\x -> email (payload x :: RegisterUserPayload)) <$> resultOfPayload
+    let resultOfUuid = uuid <$> resultOfPayload
+    case (resultOfEmail, resultOfUuid) of
+      (Aeson.Success email, Aeson.Success uuid) -> do
+        liftIO $ runQuery $ insertEvent uuid (UserRegistered email)
         setStatus HttpTypes.status201
       _ -> setStatus HttpTypes.status400
+
+  
+  post "notes" $ do
+    body <- jsonBody
+    let resultOfPayload = case body of
+          Just validJson -> Aeson.fromJSON @(WithUuid NoteAddedPayload) validJson
+          _ -> fail "Json body expected. Status 400" -- TODO: fail with 400
+    let resultOfContent = (\x -> content (payload x :: NoteAddedPayload)) <$> resultOfPayload
+    let resultOfUuid = uuid <$> resultOfPayload
+    case (resultOfContent, resultOfUuid) of
+      (Aeson.Success content, Aeson.Success uuid) -> do
+        liftIO $ runQuery $ insertEvent uuid (NoteAdded 1 content)
+        setStatus HttpTypes.status201
+      _ -> setStatus HttpTypes.status400
+      
