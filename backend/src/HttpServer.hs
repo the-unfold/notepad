3738ece293608542c04ef8dcfe152a33920b2816
Vladimir Logachev <logachev.dev@ya.ru>
2021-08-28 01:58:50 +0700

    Apply formatting

--- backend/src/HttpServer.hs
+++ backend/src/HttpServer.hs
@@ -2,24 +2,23 @@
 
 module HttpServer (handleRequests) where
 
-import HttpTypes 
 import Control.Concurrent.STM.TBChan (TBChan)
 import Control.Monad.Trans (MonadIO (liftIO))
-import Network.Wai.Handler.Warp (run)
-import Network.Wai.Middleware.Cors (cors, corsRequestHeaders, simpleCorsResourcePolicy)
-import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Data.Int (Int32)
 import DomainEvent qualified
 import EventRegistrator (insertEvent)
-import Servant
+import HttpTypes
+import Network.Wai.Handler.Warp (run)
+import Network.Wai.Middleware.Cors (cors, corsRequestHeaders, simpleCorsResourcePolicy)
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Queries qualified
+import Servant
 
 handleRequests :: TBChan () -> IO ()
 handleRequests chan = do
   let port = (8000 :: Int)
   putStrLn $ "Starting server at port " <> show port
   run port (app chan)
-
 
 app :: TBChan () -> Application
 app chan =
@@ -35,20 +34,20 @@ notepadApi = Proxy
 
 type NotepadApi =
   "users" :> ReqBody '[JSON] (WithUuid UserRegisterPayload) :> PostNoContent
-  :<|> "notes" :> Get '[JSON] [Note]
-  :<|> "notes" :> QueryParam' '[Required] "id" Int32 :> Get '[JSON] Note
-  :<|> "notes" :> "create" :> ReqBody '[JSON] (WithUuid NoteCreatePayload) :> PostNoContent
-  :<|> "notes" :> "update" :> ReqBody '[JSON] (WithUuid NoteUpdatePayload) :> PostNoContent
+    :<|> "notes" :> Get '[JSON] [Note]
+    :<|> "notes" :> QueryParam' '[Required] "id" Int32 :> Get '[JSON] Note
+    :<|> "notes" :> "create" :> ReqBody '[JSON] (WithUuid NoteCreatePayload) :> PostNoContent
+    :<|> "notes" :> "update" :> ReqBody '[JSON] (WithUuid NoteUpdatePayload) :> PostNoContent
 
 server :: TBChan () -> Server NotepadApi
-server chan = 
+server chan =
   registerUser
     :<|> getNotes
     :<|> getNote
     :<|> createNote
     :<|> updateNote
   where
-    registerUser :: (WithUuid UserRegisterPayload) -> Handler NoContent
+    registerUser :: WithUuid UserRegisterPayload -> Handler NoContent
     registerUser WithUuid {uuid, payload} = do
       liftIO $ insertEvent chan uuid (DomainEvent.UserRegistered (email payload))
       pure NoContent
@@ -57,20 +56,20 @@ server chan =
     getNotes = do
       let userId = 1
       liftIO $ Queries.queryNotes userId
-    
+
     getNote :: Int32 -> Handler Note
     getNote noteId = do
       let userId = 1
       liftIO $ Queries.queryNote noteId userId
 
-    createNote :: (WithUuid NoteCreatePayload) -> Handler NoContent
+    createNote :: WithUuid NoteCreatePayload -> Handler NoContent
     createNote WithUuid {uuid, payload} = do
       let userId = 1
-      liftIO $ insertEvent chan uuid (DomainEvent.NoteCreated userId (content (payload :: NoteCreatePayload) ))
+      liftIO $ insertEvent chan uuid (DomainEvent.NoteCreated userId (content (payload :: NoteCreatePayload)))
       pure NoContent
 
-    updateNote :: (WithUuid NoteUpdatePayload) -> Handler NoContent
+    updateNote :: WithUuid NoteUpdatePayload -> Handler NoContent
     updateNote WithUuid {uuid, payload} = do
-      let userId = 1 
+      let userId = 1
       liftIO $ insertEvent chan uuid (DomainEvent.NoteUpdated userId (noteId (payload :: NoteUpdatePayload)) (content (payload :: NoteUpdatePayload)))
       pure NoContent
--- backend/src/HttpTypes.hs
+++ backend/src/HttpTypes.hs
@@ -6,14 +6,14 @@
 module HttpTypes where
 
 import Data.Aeson qualified as Aeson
+import Data.Int (Int32)
 import Data.Text (Text)
 import Data.UUID qualified as UUID
-import Utils.ElmDeriving (ElmType)
-import Generics.SOP qualified as SOP
 import GHC.Generics (Generic)
+import Generics.SOP qualified as SOP
 import Language.Elm.Definition (Definition)
 import Language.Haskell.To.Elm qualified as E
-import Data.Int (Int32)
+import Utils.ElmDeriving (ElmType)
 
 -- |
 -- List of type definitions to be written to .elm files
@@ -34,23 +34,27 @@ data WithUuid a = WithUuid {payload :: a, uuid :: UUID.UUID}
 newtype UserRegisterPayload = UserRegisterPayload {email :: Text}
   deriving stock (Eq, Show, Read, Generic)
   deriving anyclass (Aeson.ToJSON, Aeson.FromJSON, SOP.Generic, SOP.HasDatatypeInfo)
-  deriving (E.HasElmType, E.HasElmDecoder Aeson.Value, E.HasElmEncoder Aeson.Value)
+  deriving
+    (E.HasElmType, E.HasElmDecoder Aeson.Value, E.HasElmEncoder Aeson.Value)
     via ElmType "Api.UserRegisterPayload.UserRegisterPayload" UserRegisterPayload
 
 newtype NoteCreatePayload = NoteCreatePayload {content :: Text}
   deriving stock (Eq, Show, Read, Generic)
   deriving anyclass (Aeson.ToJSON, Aeson.FromJSON, SOP.Generic, SOP.HasDatatypeInfo)
-  deriving (E.HasElmType, E.HasElmDecoder Aeson.Value, E.HasElmEncoder Aeson.Value)
+  deriving
+    (E.HasElmType, E.HasElmDecoder Aeson.Value, E.HasElmEncoder Aeson.Value)
     via ElmType "Api.NoteAddedPayload.NoteAddedPayload" NoteCreatePayload
 
 data NoteUpdatePayload = NoteUpdatePayload {noteId :: Int32, content :: Text}
   deriving stock (Eq, Show, Read, Generic)
   deriving anyclass (Aeson.ToJSON, Aeson.FromJSON, SOP.Generic, SOP.HasDatatypeInfo)
-  deriving (E.HasElmType, E.HasElmDecoder Aeson.Value, E.HasElmEncoder Aeson.Value)
+  deriving
+    (E.HasElmType, E.HasElmDecoder Aeson.Value, E.HasElmEncoder Aeson.Value)
     via ElmType "Api.NoteAddedPayload.NoteAddedPayload" NoteUpdatePayload
 
 data Note = Note {noteId :: Int32, content :: Text}
   deriving stock (Eq, Show, Read, Generic)
   deriving anyclass (Aeson.ToJSON, Aeson.FromJSON, SOP.Generic, SOP.HasDatatypeInfo)
-  deriving (E.HasElmType, E.HasElmDecoder Aeson.Value, E.HasElmEncoder Aeson.Value)
+  deriving
+    (E.HasElmType, E.HasElmDecoder Aeson.Value, E.HasElmEncoder Aeson.Value)
     via ElmType "Api.Note.Note" Note
